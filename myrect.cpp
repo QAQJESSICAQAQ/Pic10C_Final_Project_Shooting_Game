32feb57b19292d921d7195bb20dff442727b1d39
Jinqi <songjinqijess@gmail.com>
2019-03-22 20:33:19 -0700

    this time we use QKeyevent to move the rectangle up/down, and left/right

--- myrect.cpp
+++ myrect.cpp
@@ -1,12 +1,20 @@
 #include "myrect.h"
 
-#include <QDebug>
+#include <QKeyEvent>
 
-void MyRect::keyPressEvent(QKeyEvent *event)
-
-{
-
-    qDebug() << "myrec knows that you pressed the keyboard";
+void MyRect::keyPressEvent(QKeyEvent *event){
+    if (event->key()==Qt::Key_Left){
+        setPos(x()-10,y());
+    }
+    if (event->key()==Qt::Key_Right){
+        setPos(x()+10,y());
+    }
+    if (event->key()==Qt::Key_Up){
+        setPos(x(),y()-10);
+    }
+    if (event->key()==Qt::Key_Left){
+        setPos(x(),y()+10);
+    }
 
 }
 
