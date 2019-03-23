6495de1d46b8e7ee55f2951cae0dc467e8d58bd7
Jinqi <songjinqijess@gmail.com>
2019-03-22 22:09:17 -0700

    we changed rec to player to make it more like a game. We also fixed the size of the view and the player's initial position. We also attempted to solve the previous problem of the bullet going out of the view and not being deleted. However this time it is being deleted a little bit sonner than we thought.

--- bullet.cpp
+++ bullet.cpp
@@ -1,5 +1,7 @@
 #include "bullet.h"
 #include<QTimer>
+#include<QGraphicsScene>
+#include<QDebug>
 
 Bullet::Bullet(){
     setRect(0,0,10,50);//created bullet size
@@ -15,5 +17,10 @@ Bullet::Bullet(){
 void Bullet::move()
 {//move bullet up
     setPos(x(),y()-10);
-
+    if (pos().y()<0){//we delete the bullets when we can not longer see them in the window
+        //remove the bullet from the scene before deleting it
+        scene()->removeItem(this);
+        delete this;
+        qDebug() << "bullet deleted";
+    }
 }
--- main.cpp
+++ main.cpp
@@ -20,15 +20,15 @@ int main(int argc, char *argv[])
 
    //creating an item to put into the scene
 
-    MyRect * rect = new MyRect();
+    MyRect * player = new MyRect();
 
     //by default the rec has the length and width of zero
 
-    rect->setRect(0,0,100,100); //set the size
+    player->setRect(0,0,100,100); //set the size
 
     //add the item to the scene
 
-    scene->addItem(rect);
+    scene->addItem(player);
 
 
 
@@ -36,9 +36,9 @@ int main(int argc, char *argv[])
 
     //make rect focusable
 
-    rect->setFlag(QGraphicsItem::ItemIsFocusable);
+    player->setFlag(QGraphicsItem::ItemIsFocusable);
 
-    rect->setFocus();
+    player->setFocus();
 
 
 
@@ -47,10 +47,19 @@ int main(int argc, char *argv[])
     //add a view
 
     QGraphicsView * view = new QGraphicsView(scene);
+
+
     //quit solution to the "scroll bar getting smaller and smaller problem, as the bullet kept going upward
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+
+    //show the view & set a fixed  & with equal size of the view
     view->show();
+    view->setFixedSize(800, 600);
+    scene->setSceneRect(0,0,800,600);
+    //setting the player to be at the middle of the bottom
+    player->setPos(view->width()/2-50, view->height() - player->rect().height());
 
     return a.exec();
 
